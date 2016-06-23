6eb81380f39f45e0128a5cf6becde15a311036ca
Matteo Padova <padova.matteo@gmail.com>
2016-06-23 16:21:10 +0200

    scaffolding

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+ 
+gulp.task('index', function () {
+  var target = gulp.src('src/index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths: 
+  var sources = gulp.src(['bower_components/jquery/dist/jquery.min.js',
+						  'bower_components/bootstrap/dist/css/bootstrap.css',
+						  'bower_components/bootstrap/dist/js/bootstrap.min.js',
+ 						  'bower_components/gsap/src/minified/TweenMax.min.js'], {read: false});
+ 
+  return target.pipe(inject(sources)).pipe(gulp.dest('.'));
+});
